2c2aec8d78855441fb5defba725b6eca0df778f7
MoeRayo <moronfolu.motunrayo1@gmail.com>
2022-04-17 22:50:03 +0100

    feat: addition of the Vue barcode generator functionality

--- pages/index.vue
+++ pages/index.vue
@@ -3,10 +3,35 @@
     <header class="tc pv3 pv3-ns">
       <h1 class="mt2 mb0 baskerville fw1 f2-ns f3">Contact Card Barcode Generator</h1>
     </header>
-    
+
+    <div class="flex flex-wrap justify-between">
+      <form class="tc w-50-ns w-50 mt4">
+        <label for="contact-name" class="db tl">Contact name</label>
+        <input id="contact-name" v-model="contactName" type="text" name="contact-name" class="db pa2 w-30-l w-70-m w-100 mt2 br2 ba b--black-50">
+
+        <label for="contact-number" class="db tl mt4">Contact number</label>
+        <input id="contact-number" v-model="contactNumber" type="text" number="contact-number" class="db pa2 w-30-l w-70-m w-100 mt2 br2 ba b--black-50">
+      </form>
+
+      <section>
+        <VueBarcode ref="barcode" :value="contactName + contactNumber" tag="img" format="CODE128" :options="options" class="db center tc mv3"></VueBarcode>
+      </section>
+    </div>
   </div>
 </template>
 
 <script>
-export default {}
+  import VueBarcode from '@chenfengyuan/vue-barcode';
+
+export default {
+  components: {
+    VueBarcode
+  },
+  data(){
+    return {
+      contactName: '',
+      contactNumber: ' '
+    }
+  }, 
+}
 </script>
