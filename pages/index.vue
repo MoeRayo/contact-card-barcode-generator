61d3ea491b8998cbb226090d98e5b85bbab0c59b
MoeRayo <moronfolu.motunrayo1@gmail.com>
2022-04-17 21:46:44 +0100

    feat: header creation

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,8 @@
 <template>
-  <div>
+  <div class="ph4 pv3 bg-washed-red vh-100">
+    <header class="tc pv3 pv3-ns">
+      <h1 class="mt2 mb0 baskerville fw1 f2-ns f3">Contact Card Barcode Generator</h1>
+    </header>
     
   </div>
 </template>
